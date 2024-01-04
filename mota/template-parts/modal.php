c4d8732e28869db330d1653dcadcafb7e827448b
Tali Marouf <tali.marouf@outlook.fr>
2024-01-05 00:11:17 +0100

    Ajouter le fichier modal.php

---
+++ mota/template-parts/modal.php
@@ -0,0 +1,17 @@
+ <!-- The Modal -->
+<div id="myModal" class="modal fade-in">
+
+ <!-- Modal content -->
+  <div class="modal-content fade-in">
+    <div class="modal-header">
+       <img src="<?php echo get_template_directory_uri() ?>/assets/images/img-modal.png" alt="image du formulaire de contact" id="img-modal" />
+    </div>
+    <div id="contact-form-container">
+        <?php // insère le formulaire de contact
+            echo do_shortcode('[contact-form-7 id="4460a13" title="Formulaire de contact"]');
+        ?>  
+    </div>
+  </div>
+</div>
+
+
